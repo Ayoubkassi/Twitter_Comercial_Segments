5ce9a976421a6a3b367e8cc67af6e16b59b5eb33
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2023-03-10 01:05:47 +0100

    add mongodb and mysql images and scripts

--- util/csv_to_mysql.py
+++ util/csv_to_mysql.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 
 # Read the CSV file into a pandas dataframe
-df = pd.read_csv('../apple.csv')
+df = pd.read_csv('../i_want_an_iphone_after_+10500.csv')
 # print(df)
 
 # Connect to a MySQL database
@@ -20,12 +20,14 @@ conn = mysql.connector.connect(
 
 # Convert the dataframe to a MySQL table
 cursor = conn.cursor()
-cursor.execute('DROP TABLE IF EXISTS apple')
+cursor.execute('DROP TABLE IF EXISTS Want_iphone')
 cursor.execute(
-    'CREATE TABLE apple (Username VARCHAR(16), Name VARCHAR(50), PostDate VARCHAR(25), TweetText VARCHAR(424), ReplyCount VARCHAR(25),RetweetCount VARCHAR(25),LikeCount VARCHAR(25),Views VARCHAR(25), gender VARCHAR(10) )')
+    'CREATE TABLE Want_iphone (Username VARCHAR(16), Name VARCHAR(50), PostDate VARCHAR(25), TweetText VARCHAR(424), ReplyCount VARCHAR(25),RetweetCount VARCHAR(25),LikeCount VARCHAR(25),Views VARCHAR(25) )')
 for row in df.itertuples():
-    cursor.execute(
-        'INSERT INTO apple (Username, Name, PostDate, TweetText , ReplyCount ,RetweetCount ,LikeCount ,Views , gender ) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s )', tuple(row[1:]))
+    if row != None:
+        print(1)
+        cursor.execute(
+            'INSERT INTO Want_iphone (Username, Name, PostDate, TweetText , ReplyCount ,RetweetCount ,LikeCount ,Views ) VALUES (%s, %s, %s,%s, %s, %s,%s, %s )', tuple(row[1:]))
 conn.commit()
 
 # Close the connection
--- util/json_to_mongodb.py
+++ util/json_to_mongodb.py
@@ -4,16 +4,22 @@ from pymongo import MongoClient
 # Connect to the MongoDB database
 client = MongoClient('mongodb://localhost:27017/')
 db = client['twitterScrap']
-collection = db['users']
 
-# Read the JSON file
-with open('../apple_users.json') as f:
-    decoder = json.JSONDecoder()
-    data = f.read()
-    while data:
-        obj, idx = decoder.raw_decode(data)
-        data = data[idx:].lstrip()
-        collection.insert_one(obj)
 
-# Close the MongoDB connection
-client.close()
+with open('../i_want_an_iphone_after_+10500_users_with_gender.json') as f:
+    data = json.load(f)
+
+
+collection = db['iphone_users']
+result = collection.insert_many(data)
+# # Read the JSON file
+# with open('../i_want_an_iphone_after_+10500_users_with_gender.json') as f:
+#     decoder = json.JSONDecoder()
+#     data = f.read()
+#     while data:
+#         obj, idx = decoder.raw_decode(data)
+#         data = data[idx:].lstrip()
+#         collection.insert_one(obj)
+
+# # Close the MongoDB connection
+# client.close()
